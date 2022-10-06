e32145d6b095510e197fd872746a4f3518cd2277
Andrew-Neely-82 <andrew.neely82@gmail.com>
2022-10-06 15:37:21 -0500

    updated

--- js/external.js
+++ js/external.js
@@ -11,6 +11,8 @@ alert(favoriteColor + " is mine too!");
 
 // Question 3
 
+// 3.1
+
 //You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it),
 // and Hercules (1 day, you don't know yet if they're going to like it). If price for a movie per day is $3, how much
 // will you have to pay?
@@ -24,6 +26,8 @@ let hercules = prompt("How many days do you want to rent Hercules?");
 let movieRentalTotal = (pricePerDay * mermaid) + (pricePerDay * bear) + (pricePerDay * hercules);
 console.log("Movie rent total is $" + movieRentalTotal)
 alert("Your total for renting is $" + movieRentalTotal);
+
+// 3.2
 
 // Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate
 // per hour. Google pays $400, Amazon $380, and Facebook $350. How much will you receive in payment for this week? You
@@ -40,6 +44,8 @@ let totalPay = (googlePerHour * googleHoursWorked) + (amazonPerHour * amazonHour
 console.log("Wages earned is $" + totalPay);
 alert("You made $" + totalPay + " this week");
 
+// 3.3
+
 // A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with
 // her current schedule.
 
@@ -49,6 +55,8 @@ let schedule = confirm("Does this interfere with your schedule")
 if (fullClass === false && schedule === false) {
     alert("You can be enrolled in this class")
 } else alert("You can not be enrolled in this class")
+
+// 3.4
 
 // A product offer can be applied only if a person buys more than 2 items, and the offer has not expired. Premium
 // members do not need to buy a specific amount of products.
