ab44b8da31d5230f9ba0ee98039977df88c677b5
Andrew-Neely-82 <andrew.neely82@gmail.com>
2022-10-12 11:24:53 -0500

    condtionals js - done

--- js/conditionals.js
+++ js/conditionals.js
@@ -28,35 +28,42 @@
 
 
 
-function analyzeColor() {
+function analyzeColor(color) {
     if (color === "red") {
-        return alert("\"red\" is the color for hot");
+        return "Q1: " + color  + " is the color for hot";
     }
-    if (color === "orange") {
-        return alert("\"orange\" is for road cones");
+    else if (color === "orange") {
+        return "Q1: " + color  + " is for road cones";
     }
-    if (color === "yellow") {
-        return alert("\"yellow\" is for the sun");
+    else if (color === "yellow") {
+        return "Q1: " + color  + " is for the sun";
     }
-    if (color === "green") {
-        return alert("\"green\" is for grass");
+    else if (color === "green") {
+        return "Q1: " + color  + " is for grass";
     }
-    if (color === "blue") {
-        return alert("\"blue\" is for water");
+    else if (color === "blue") {
+        return "Q1: " + color  + " is for water";
     }
-    if (color === "indigo") {
-        return alert("\"indigo\" is for airline tickets");
+    else if (color === "indigo") {
+        return "Q1: " + color + " is for airline tickets";
     }
-    if (color === "violet") {
-        return alert("\"violet\" is for the girl in willy wonka");
+    else if (color === "violet") {
+        return "Q1: " + color + " is for the girl in willy wonka";
     }
     else {
-        return alert("I don't know anything about \"cyan\"");
+        return "Q1: I don't know anything about " + color;
     }
 }
 
-let color = prompt(" \rType in a color: \r\r" + "red, orange, yellow, green, blue, indigo, violet, or cyan")
-analyzeColor(console.log("Q1: " + "User chose " + color));
+console.log(analyzeColor("red"));
+console.log(analyzeColor("orange"));
+console.log(analyzeColor("yellow"));
+console.log(analyzeColor("green"));
+console.log(analyzeColor("blue"));
+console.log(analyzeColor("indigo"));
+console.log(analyzeColor("violet"));
+
+
 
 
 
@@ -65,8 +72,10 @@ analyzeColor(console.log("Q1: " + "User chose " + color));
 // - `colors`: a list of the colors of the rainbow
 // - `randomColor`: contains a single random color value from the list (this
 //                  will contain a different color every time the page loads)
+
 let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 let randomColor = colors[Math.floor(Math.random() * colors.length)];
+
 /**
  * TODO: Q2
  * Pass the `randomColor` variable to your 'analyzeColor' function and console.log the results.
@@ -75,8 +84,7 @@ let randomColor = colors[Math.floor(Math.random() * colors.length)];
 
 
 
-//
-
+//console.log(analyzeColor(randomColor));
 
 
 /**
@@ -85,7 +93,32 @@ let randomColor = colors[Math.floor(Math.random() * colors.length)];
  */
 
 
-// Start here
+ switch(randomColor) {
+     case "red":
+         console.log("Q3: red is the color for hot");
+         break;
+     case "orange":
+         console.log("Q3: blue is for road cones");
+         break;
+     case "yellow":
+         console.log("Q3: yellow is for the sun")
+         break;
+     case "green":
+         console.log("Q3: green is for grass");
+         break;
+     case "blue":
+         console.log("Q3: blue is for water");
+         break;
+     case "indigo":
+         console.log("Q3: indigo is for airline tickets");
+         break;
+     case "violet":
+         console.log("Q3: violet is for airline tickets");
+         break;
+     default:
+         console.log("Q3: I don't know anything about cyan");
+         break;
+ }
 
 
 /**
@@ -96,7 +129,10 @@ let randomColor = colors[Math.floor(Math.random() * colors.length)];
  */
 
 
-// let analyzeColor = prompt("Tell me a color!");
+let userColor = prompt("Choose a color: red, orange, yellow, green, blue, indigo, or cyan").toLowerCase();
+
+alert(analyzeColor(userColor));
+
 
 
 /* ########################################################################## */
@@ -123,12 +159,41 @@ let randomColor = colors[Math.floor(Math.random() * colors.length)];
 
 
 
-//
+function calculateTotal(luckyNumber, price) {
+    if (luckyNumber === 0) {
+        return console.log("Q5: $" + (price - (0)).toFixed(2))
+    }
+    else if (luckyNumber === 1) {
+        return console.log("Q5: $" + (price - (price * 0.1)).toFixed(2));
+        }
+    else if (luckyNumber === 2) {
+        return console.log("Q5: $" + (price - (price * 0.25)).toFixed(2));
+    }
+    else if (luckyNumber === 3) {
+        return console.log("Q5: $" + (price - (price * 0.35)).toFixed(2));
+    }
+    else if (luckyNumber === 4) {
+        return console.log("Q5: $" + (price - (price * 0.5)).toFixed(2));
+    }
+    else {
+        return console.log("Q5: It's FREE!!!")
+    }
+}
+
+//let price = Number(prompt("How much does it cost?"))
+//let luckyNumber = Number(prompt("What's your lucky number? \r\r 0-5"))
+
+calculateTotal(0, 100);
+calculateTotal(1, 100);
+calculateTotal(2, 100);
+calculateTotal(3, 100);
+calculateTotal(4, 100);
+calculateTotal(5, 100);
 
 
 
 /**
- * TODO: Q6
+ * TODO:
  * Uncomment the line below to generate a random number between 0 and 5.
  * (In this line of code, 0 is inclusive, and 6 is exclusive)
  * Prompt the user for their total bill, then use your `calculateTotal` function
@@ -136,7 +201,12 @@ let randomColor = colors[Math.floor(Math.random() * colors.length)];
  * price before the discount was, and what their price after the discount is.
  */
 // Generate a random number between 0 and 6
-// var luckyNumber = Math.floor(Math.random() * 6);
+let luckyNumber = Math.floor(Math.random() * 6);
+let bill = parseInt(prompt("Whats your total bill?"));
+
+let priceAfter = calculateTotal(luckyNumber, bill);
+
+alert("Your lucky number is " + luckyNumber + " Price before discount is " + bill + " and price after is " + priceAfter);
 
 /**
  * TODO: Q7
@@ -148,8 +218,7 @@ let randomColor = colors[Math.floor(Math.random() * colors.length)];
  * - what the number plus 100 is
  * - if the number is negative or positive
  *
- * Do *NOT* display any of the above information
- * if the user enters a value that is not of the number data type.
+ * Do *NOT* display any of the above information if the user enters a value that is not of the number data type.
  * Instead, use an alert to inform them of the incorrect input data type.
  *
  *
@@ -158,7 +227,50 @@ let randomColor = colors[Math.floor(Math.random() * colors.length)];
  */
 
 
-//
+function askToPlay() {
+    let letsPlay = confirm("Would you like to enter a number?");
+    if (letsPlay === false) {
+        alert("ok then...")
+    }
+    else if (letsPlay === true) {
+        const userInput = Number(prompt("Enter a number"));
+        isNumber(userInput)
+    }
+}
+
+function isOddOrEven(x) {
+    if (x % 2 === 0) {
+        alert(x + " is Even!")
+    }
+    else alert(x + " is Odd!")
+}
+
+function plus100(x) {
+    alert("100 + " + x + " is " + (x + 100))
+}
+
+function isPositiveOrNegative(x) {
+    if (x > 0) {
+        alert(x + " is Positive!")
+    }
+    else alert(x + " is Negative!")
+}
+
+function isNumber(x) {
+    if (isNaN(x)) {
+        alert("That is not a number data type!");
+    }
+    else if (!isNaN(x)) {
+        isOddOrEven(x);
+        plus100(x);
+        isPositiveOrNegative(x);
+    }
+}
+
+
+askToPlay();
+
+
 
 
 // })();
