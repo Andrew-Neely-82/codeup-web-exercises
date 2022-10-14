f8776371db67cd35cca84e907e33a9a327024ecd
Andrew-Neely-82 <andrew.neely82@gmail.com>
2022-10-14 15:43:37 -0500

    added

--- js/while.js
+++ js/while.js
@@ -16,18 +16,18 @@ console.log(``);
 // TODO Q1 --- DO WHILE ---
 
 function iceCreamCones(){
-    let conesLeft =  Math.floor(Math.random() * 50) + 50;
+    let conesAvailable =  Math.floor(Math.random() * 50) + 50;
     do {
         let orders = Math.floor(Math.random() * 5) + 1;
-        if (orders > conesLeft){
-            console.log(`There's only ${conesLeft} cones left...`);
+        if (orders > conesAvailable) {
+            console.log(`There's only ${conesAvailable} cones left...`);
         }
-        else{
-            conesLeft -= orders;
-            console.log(`You ordered ${orders} cones, we have ${conesLeft} left.`)
+        else {
+            conesAvailable -= orders;
+            console.log(`You ordered ${orders} ice cream cones, we have ${conesAvailable} left.`)
         }
     }
-    while (conesLeft > 0);
+    while (conesAvailable > 0);
     console.log(`Yay! I sold them all!`)
 }
 
