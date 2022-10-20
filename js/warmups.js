5cf69b8c0367c8fefa02ccc38cbd8a1be7914bd8
Andrew-Neely-82 <andrew.neely82@gmail.com>
2022-10-20 11:12:42 -0500

    added

--- js/warmups.js
+++ js/warmups.js
@@ -1,11 +1,19 @@
+// TODO Warmup 1
 // Write a function that when passed an object will return the value of the object’s price property.
 // Ex. let obj = {name: “Potatoes”, quantity: 2, price: “$7.89"};
 // getPrice(obj); // returns “$7.89”
 
-let obj = {name: `Potatoes`, quantity: 2, price: `$7.89`}
+// let obj = {name: `Potatoes`, quantity: 2, price: `$7.89`}
+//
+// function getPrice(obj) {
+//     return obj.price;
+// }
+//
+// console.log(getPrice(obj));
 
-function getPrice(obj) {
-    return obj.price;
-}
 
-console.log(getPrice(obj));
+// =====================================================================================================================
+
+// TODO Warmup 2
+
+
