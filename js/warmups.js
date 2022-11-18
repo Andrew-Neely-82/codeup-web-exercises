b649026a022a88aba252c77bc7f165347484bb52
Andrew-Neely-82 <andrew.neely82@gmail.com>
2022-11-18 14:11:33 -0600

    added iife

--- js/warmups.js
+++ js/warmups.js
@@ -1,125 +1,126 @@
-// TODO Warmup 1
-// Write a function that when passed an object will return the value of the object’s price property.
-// Ex. let obj = {name: “Potatoes”, quantity: 2, price: “$7.89"};
-// getPrice(obj); // returns “$7.89”
+(() => {
+  `use strict`;
 
-// let obj = {name: `Potatoes`, quantity: 2, price: `$7.89`}
-//
-// function getPrice(obj) {
-//     return obj.price;
-// }
-//
-// console.log(getPrice(obj));
+  // TODO Warmup 1
+  // Write a function that when passed an object will return the value of the object’s price property.
+  // Ex. let obj = {name: “Potatoes”, quantity: 2, price: “$7.89"};
+  // getPrice(obj); // returns “$7.89”
 
+  // let obj = {name: `Potatoes`, quantity: 2, price: `$7.89`}
+  //
+  // function getPrice(obj) {
+  //     return obj.price;
+  // }
+  //
+  // console.log(getPrice(obj));
 
-// =====================================================================================================================
+  // =====================================================================================================================
 
-// // TODO Warmup 2
-//
-// function longestString(array) {
-//     let longest = '';
-//     array.forEach(function(string) {
-//         if (string.length > longest.length) {
-//             longest = string;
-//         }
-//     });
-//     return console.log(longest);
-// }
-//
-// let array = ['Audi', 'BMW', 'Citroen', 'Dacia', 'Ferrari', 'Mercedes', 'Porsche', 'Volkswagen'];
-//
-// longestString(array);
+  // // TODO Warmup 2
+  //
+  // function longestString(array) {
+  //     let longest = '';
+  //     array.forEach(function(string) {
+  //         if (string.length > longest.length) {
+  //             longest = string;
+  //         }
+  //     });
+  //     return console.log(longest);
+  // }
+  //
+  // let array = ['Audi', 'BMW', 'Citroen', 'Dacia', 'Ferrari', 'Mercedes', 'Porsche', 'Volkswagen'];
+  //
+  // longestString(array);
 
-// =====================================================================================================================
+  // =====================================================================================================================
 
-// TODO Warmup 3
+  // TODO Warmup 3
 
-const hamsters = [
+  const hamsters = [
     {
-        name: "Hamtaro",
-        heightInMM: 86,
-        fur: ['orange', 'white'],
-        gender: "male",
-        dateOfBirth: "August 6"
-    } , {
-        name: "Bijou",
-        heightInMM: 75,
-        fur: ['white'],
-        gender: "female",
-        dateOfBirth: "July 10"
-    } , {
-        name: "Oxnard",
-        heightInMM: 100,
-        fur: ['grey', 'white'],
-        gender: "male",
-        dateOfBirth: "May 3"
-    } , {
-        name: "Boss",
-        heightInMM: 120,
-        fur: ['brown', 'white'],
-        gender: "male",
-        dateOfBirth: "September 21"
-    } , {
-        name: "Snoozer",
-        heightInMM: 85,
-        fur: ['brown', 'white', "pink"],
-        gender: "male",
-        dateOfBirth: "January 14"
-    }
-];
+      name: "Hamtaro",
+      heightInMM: 86,
+      fur: ["orange", "white"],
+      gender: "male",
+      dateOfBirth: "August 6",
+    },
+    {
+      name: "Bijou",
+      heightInMM: 75,
+      fur: ["white"],
+      gender: "female",
+      dateOfBirth: "July 10",
+    },
+    {
+      name: "Oxnard",
+      heightInMM: 100,
+      fur: ["grey", "white"],
+      gender: "male",
+      dateOfBirth: "May 3",
+    },
+    {
+      name: "Boss",
+      heightInMM: 120,
+      fur: ["brown", "white"],
+      gender: "male",
+      dateOfBirth: "September 21",
+    },
+    {
+      name: "Snoozer",
+      heightInMM: 85,
+      fur: ["brown", "white", "pink"],
+      gender: "male",
+      dateOfBirth: "January 14",
+    },
+  ];
 
-// getTallestHamster(hamsters); // should return {name: "Boss", heightInMM: 120, fur: ['brown', 'white'], gender: "male", dateOfBirth: "September 21"};
-//
-// function getTallestHamster(hamsters) {
-//     let tallestHamster = {heightInMM: 0};
-//     hamsters.forEach(function(hamster){
-//         if (hamster.heightInMM > tallestHamster.heightInMM) {
-//             tallestHamster = hamster;
-//         }
-//     });
-//     return tallestHamster;
-// }
-//
-// console.log(getTallestHamster(hamsters));
+  // getTallestHamster(hamsters); // should return {name: "Boss", heightInMM: 120, fur: ['brown', 'white'], gender: "male", dateOfBirth: "September 21"};
+  //
+  // function getTallestHamster(hamsters) {
+  //     let tallestHamster = {heightInMM: 0};
+  //     hamsters.forEach(function(hamster){
+  //         if (hamster.heightInMM > tallestHamster.heightInMM) {
+  //             tallestHamster = hamster;
+  //         }
+  //     });
+  //     return tallestHamster;
+  // }
+  //
+  // console.log(getTallestHamster(hamsters));
 
+  // TODO: FizzBuzz from 1-100;
+  // Numbers evenly divisible by 3 should be replaced by Fizz
+  // Numbers evenly divisible by 5 should be replaced by Buzz
+  // Numbers evenly divisible by both 3 and 5 should be replaced by FizzBuzz
+  // All other numbers should be printed in the console.
 
+  // function fizzBuzz() {
+  //     for (let i = 1; i <= 100; i++) {
+  //         if (i % 3 === 0 && i % 5 === 0) {
+  //             console.log(`FizzBuzz`);
+  //         } else if (i % 3 === 0) {
+  //             console.log(`Fizz`);
+  //         } else if (i % 5 === 0) {
+  //             console.log(`Buzz`);
+  //         } else {
+  //             console.log(i);
+  //         }
+  //     }
+  // }
+  //
+  // fizzBuzz();
+  //
 
-// TODO: FizzBuzz from 1-100;
-// Numbers evenly divisible by 3 should be replaced by Fizz
-// Numbers evenly divisible by 5 should be replaced by Buzz
-// Numbers evenly divisible by both 3 and 5 should be replaced by FizzBuzz
-// All other numbers should be printed in the console.
+  // TODO: Today's warmup: Write a function that takes in an array of objects and
+  // returns the average of the heightInMM properties from each object.
 
-
-
-// function fizzBuzz() {
-//     for (let i = 1; i <= 100; i++) {
-//         if (i % 3 === 0 && i % 5 === 0) {
-//             console.log(`FizzBuzz`);
-//         } else if (i % 3 === 0) {
-//             console.log(`Fizz`);
-//         } else if (i % 5 === 0) {
-//             console.log(`Buzz`);
-//         } else {
-//             console.log(i);
-//         }
-//     }
-// }
-//
-// fizzBuzz();
-//
-
-
-// TODO: Today's warmup: Write a function that takes in an array of objects and
-// returns the average of the heightInMM properties from each object.
-
-
-function getAverageHeight(array) {
+  function getAverageHeight(array) {
     let total = 0;
     array.forEach((hamster) => {
-        total += hamster.heightInMM;
+      total += hamster.heightInMM;
     });
     console.log(total / array.length);
-}
+  }
 
-getAverageHeight(hamsters);
+  getAverageHeight(hamsters);
+})();
