1befa216302b78389ff46586900dd3e64b8c3bf8
Andrew-Neely-82 <andrew.neely82@gmail.com>
2022-11-18 14:00:24 -0600

    added iife

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,30 +1,32 @@
 "use strict";
 
-// TODO --- BREAK AND CONTINUE ---
+(() => {
+  // TODO --- BREAK AND CONTINUE ---
 
+  // TODO Q1
 
-// TODO Q1
-
-function skipOddNumbersAnd27(){
+  function skipOddNumbersAnd27() {
     let x;
-    while (true){
-        x = parseFloat(prompt(`Type a number from 1-50 that is odd`))
-        if (x % 2 !==0 && x >=1 && x <= 50){
-            break;
-        }
+    while (true) {
+      x = parseFloat(prompt(`Type a number from 1-50 that is odd`));
+      if (x % 2 !== 0 && x >= 1 && x <= 50) {
+        break;
+      }
     }
-    for (let i = 1; i < 50; i++){
-        if (x === i){
-            console.log(`Yikes, Skipping: ${x}`)
-            continue;
-        }
-        else if (i%2 ===0){
-            continue;
-        }
-        console.log(`Here is an odd number: ${i}`);
+    for (let i = 1; i < 50; i++) {
+      if (x === i) {
+        console.log(`Yikes, Skipping: ${x}`);
+        continue;
+      } else if (i % 2 === 0) {
+        continue;
+      }
+      console.log(`Here is an odd number: ${i}`);
     }
-}
+  }
 
-console.log(`============ Break and Continue - Exercise 1 ============`); console.log(``);
-skipOddNumbersAnd27();
-console.log(``); console.log(``);
+  console.log(`============ Break and Continue - Exercise 1 ============`);
+  console.log(``);
+  skipOddNumbersAnd27();
+  console.log(``);
+  console.log(``);
+})();
