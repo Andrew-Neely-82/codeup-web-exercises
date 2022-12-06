842c6ba550e14ed25d45f9c89bf9c4314ab0d9e0
Andrew-Neely-82 <andrew.neely82@gmail.com>
2022-12-06 13:54:16 -0600

    added/updated

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -38,21 +38,30 @@
       yearsOfExperience: 9,
     },
   ];
-
-  // TODO: Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
-
+  
+  // * TODO: Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
+  
   const threeLanguages = users.filter((user) => user.languages.length >= 3);
-
-  // TODO: Use .map to create an array of strings where each element is a user's email address
+  console.log(`TODO: 1`);
+  console.log(threeLanguages);
+  console.log(``);
+  
+  // * TODO: Use .map to create an array of strings where each element is a user's email address
 
   const emails = users.map((user) => user.email);
-
-  // TODO: Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
-
+  console.log(`TODO: 2`);
+  console.log(emails);
+  console.log(``);
+  
+  // * TODO: Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
+  
   const totalYears = users.reduce((total, user) => total + user.yearsOfExperience, 0);
+  console.log(`TODO: 3`);
+  console.log(totalYears);
+  console.log(``);
 
-  // TODO: Use .reduce to get the longest email from the list of users.
-
+  // * TODO: Use .reduce to get the longest email from the list of users.
+  
   const longestEmail = users.reduce((longest, user) => {
     if (user.email.length > longest.length) {
       return user.email;
@@ -60,15 +69,21 @@
       return longest;
     }
   }, "");
-
-  // TODO: Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
-
+  console.log(`TODO: 4`);
+  console.log(longestEmail);
+  console.log(``);
+  
+  // * TODO: Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
+  
   const instructors = users.reduce((names, user) => {
     return names + user.name + ", ";
   }, "Your instructors are: ");
-
-  // TODO: Use .reduce to get the unique list of languages from the list of users.
-
+  console.log(`TODO: 5`);
+  console.log(instructors);
+  console.log(``);
+  
+  // * TODO: Use .reduce to get the unique list of languages from the list of users.
+  
   const languages = users.reduce((uniqueLanguages, user) => {
     user.languages.forEach((language) => {
       if (!uniqueLanguages.includes(language)) {
@@ -77,4 +92,7 @@
     });
     return uniqueLanguages;
   }, []);
+  
+  console.log(`TODO: 6`);
+  console.log(languages);
 })();
