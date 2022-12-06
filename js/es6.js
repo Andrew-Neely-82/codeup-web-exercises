d5a31ab7560336647eb2a465f019619684cc4c4e
Andrew-Neely-82 <andrew.neely82@gmail.com>
2022-12-06 13:41:51 -0600

    added:   es6.html 	updated:   js/es6.js

--- js/es6.js
+++ js/es6.js
@@ -1,9 +1,7 @@
 `use strict`;
 
 (() => {
-  /*
-   * Complete the TODO items below
-   */
+  // * Complete the TODO items below
   const users = [
     {
       name: "zach",
@@ -32,63 +30,80 @@
     },
   ];
 
-  // fill in your name and email and add some programming languages you know
+  // * TODO: fill in your name and email and add some programming languages you know
   // to the languages array
   users.push({
     name: `andrew`,
-    email: `andrew@fakemail.com`,
-    languages: [`html`, `css`, `javascript`, `bootstrap`, `jquery`, `ajax`],
+    email: `fake@fakemail.com`,
+    languages: [`html`, `css`, `javascript`, `python`],
   });
 
-  // replace the `var` keyword with `const`, then try to reassign a variable
-  // declared as `const`
-  const name = "your_name_here";
-  const email = "";
-  const languages = [];
+  console.log(users);
 
-  // rewrite the object literal using object property shorthand
+  // * TODO: replace the `var` keyword with `const`, then try to reassign a variable
+  // declared as `const`
+  const name = `const`;
+  const email = `const@const.com`;
+  const languages = [`const`, `not var`];
+
+  // * TODO: rewrite the object literal using object property shorthand
+  // users.push({
+  //   name: name,
+  //   email: email,
+  //   languages: languages,
+  // });
   users.push({
     name,
     email,
     languages,
   });
 
-  // replace `var` with `let` in the following variable declarations
+  // * TODO: replace `var` with `let` in the following variable declarations
   let emails = [];
   let names = [];
 
-  // rewrite the following using arrow functions
-  users.forEach((user) => {
-    return emails.push(user.email);
-  });
-  users.forEach((user) => {
-    return names.push(user.name);
-  });
+  // * TODO: rewrite the following using arrow functions
+  // users.forEach(function (user) {
+  //   return emails.push(user.email);
+  // });
+  // users.forEach(function (user) {
+  //   return names.push(user.name);
+  // });
+  users.forEach((user) => emails.push(user.email));
+  users.forEach((user) => names.push(user.name));
+  console.log(emails);
 
-  // replace `var` with `let` in the following declaration
+  // * TODO: replace `var` with `let` in the following declaration
   let developers = [];
-  users.forEach(function (user) {
-    // TODO: rewrite the code below to use object destructuring assignment
+  users.forEach((user) => {
+    // * TODO: rewrite the code below to use object destructuring assignment
     //       note that you can also use destructuring assignment in the function
     //       parameter definition
+    // const name = user.name;
+    const { name } = user;
+    // const email = user.email;
+    const { email } = user;
+    // const languages = user.languages;
+    const { languages } = user;
 
-    const name = user.name;
-    const email = user.email;
-    const languages = user.languages;
-
-    // rewrite the assignment below to use template strings
+    // * TODO: rewrite the assignment below to use template strings
+    // developers.push(name + "'s email is " + email + name + " knows " + languages.join(", "));
     developers.push(`${name}'s email is ${email}. ${name} knows ${languages.join(", ")}`);
   });
 
-  // Use `let` for the following variable
+  console.log(developers);
+
+  // * TODO: Use `let` for the following variable
   let list = "<ul>";
 
-  // rewrite the following loop to use a for of loop
+  // * TODO: rewrite the following loop to use a for..of loop
   // developers.forEach(function (developer) {
   for (developer of developers) {
-    // TODO: rewrite the assignment below to use template strings
-    list += "<li>${developer}</li>";
+    // * TODO: rewrite the assignment below to use template strings
+    // list += "<li>" + developer + "</li>";
+    list += `<li>${developer}</li>`;
+    // });
   }
-
-  list += "</ul>";
+  // list += "</ul>";
+  console.log(list);
 })();
